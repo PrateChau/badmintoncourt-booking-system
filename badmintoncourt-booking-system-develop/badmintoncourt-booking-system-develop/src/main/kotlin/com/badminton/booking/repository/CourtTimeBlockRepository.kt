a6d9332f26de66323c4aa9dee7dd0e78ab142eed
Sharma <hsharma31@deloitte.com>
2026-01-03 12:08:46 +0530

    removed merge conflicts

--- badmintoncourt-booking-system-develop/badmintoncourt-booking-system-develop/src/main/kotlin/com/badminton/booking/repository/CourtTimeBlockRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.badminton.booking.repository
-import com.badminton.booking.entity.CourtTimeBlock
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface CourtTimeBlockRepository : JpaRepository<CourtTimeBlock, Long> {
-    fun findByCourtIdAndDate(courtId: Long, date: String): List<CourtTimeBlock>
-}
